35c20fd6da7b58eabeadad93e149a999fe32eeb9
Kenny Luong <luong97@hawaii.edu>
2016-02-28 22:31:41 -1000

    Added url to item

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229082727) do
+ActiveRecord::Schema.define(version: 20160229083117) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160229082727) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "part_number"
+    t.string   "url"
   end
 
 end
